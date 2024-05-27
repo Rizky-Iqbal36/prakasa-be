01774a1fb47defd7714267b30b3218518f310918
Rizky Ikbal <rizkiiqbal36@gmail.com>
2024-05-27 19:02:37 +0700

    feat: 🎸 add API login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -14,6 +15,7 @@ class AuthController extends Controller
     {
         $this->DB = DB::connection('mysql');
     }
+    
     public function register(Request $req)
     {
         $body = $req->json()->all();
@@ -26,8 +28,6 @@ class AuthController extends Controller
         $queries_validation = $this->validateReq($queries_validator);
         if(!is_null($queries_validation)) return $queries_validation;
 
-        $body['password'] = bcrypt($body['password']);
-
         $user = User::create([
             'name' => $body['name'],
             'email' => $body['email'],
@@ -37,5 +37,27 @@ class AuthController extends Controller
         $token = $user->createToken('API Token')->accessToken;
 
         return response([ 'user' => $user, 'token' => $token]);
+    }
+
+    public function login(Request $req)
+    {
+        $body = $req->json()->all();
+        $rules = [
+            'email' => 'email|required',
+            'password' => 'required'
+        ];
+        $queries_validator = Validator::make($body, $rules);
+        $queries_validation = $this->validateReq($queries_validator);
+        if(!is_null($queries_validation)) return $queries_validation;
+        
+        if (!auth()->attempt($body)) 
+            return ['error_message' => 'Invalid email or password'];
+        
+        /** @var Authenticatable $user */
+        $user = auth()->user();
+        $token = $user->createToken('API Token')->accessToken;
+
+        return ['user' => $user, 'token' => $token];
+
     }
 }
